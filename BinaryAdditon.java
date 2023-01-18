5f3b20fff2d6e77b0d12c9c78d8f2c18bf235ffd
Muskan Verma <100348028+MuskanVerma11@users.noreply.github.com>
2023-01-18 07:13:29 -0800

    Update BinaryAdditon.java

--- BinaryAdditon.java
+++ BinaryAdditon.java
@@ -7,10 +7,8 @@ class BinaryAddition{
 		int a[]=new int[2];
 		int b[]=new int[2];
 		Scanner sc=new Scanner(System.in);
-		
-    System.out.println("###Binary addition for 2 bit###");
-		
-    System.out.println("#Enter first binary number-#");
+		System.out.println("###Binary addition for 2 bit###");
+		System.out.println("#Enter first binary number-#");
 		for(j=0;j<a.length;j++){
 		System.out.println("Enter " +j+ " bit of binary number");
 		a[j]=sc.nextInt();
@@ -23,22 +21,28 @@ class BinaryAddition{
 	}
 		
 		int sum[]=new int[2];
-		
-    for(i=0;i<sum.length;i++){
+		for(i=0;i<sum.length;i++){
+			if(a[i]!=0 && a[i]!=1 || b[i]!=0 && b[i]!=1)
+			{ 
+		System.out.println("wrong inputs found , please enter binary numbers(0 & 1)");
+		}else{
 			if(a[i]==0 && b[i]==0){
 				sum[i]=0;
-	};
+
+		}
 			if(a[i]==0 && b[i]==1){
 				sum[i]=1;
-	};
+		}
 			if(a[i]==1 && b[i]==0){
 				sum[i]=1;
-	};		if(a[i]==1 && b[i]==1){
+		}	
+			if(a[i]==1 && b[i]==1){
 				sum[i]=10;
+		}
+		
 	}
-} 		
-    System.out.println("Sum of binary numbers = ");
-		for(l=0;l<sum.length;l++){
-		System.out.print(sum[l]);}
+}	System.out.println("Sum of binary numbers = "); 
+	for(l=0;l<sum.length;l++){
+		System.out.print(sum[l]);}		
 	}
 }
