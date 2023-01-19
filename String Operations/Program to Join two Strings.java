899c63094729d6fd34437683b61c10e486043a57
Muskan Verma <100348028+MuskanVerma11@users.noreply.github.com>
2023-01-19 04:20:15 -0800

    Create Program to Join two Strings.java

---
+++ String Operations/Program to Join two Strings.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class stringJoin{
+	public static void main(String [] args){
+	System.out.println("Enter your name ");
+	String str1="My name is ";
+	Scanner sc=new Scanner(System.in);
+	String str2=sc.nextLine();
+	String str=str1+str2;
+	System.out.println(str);
+	}	
+}
