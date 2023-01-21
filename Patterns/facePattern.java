15da4c0534d9e3b42bdb6fa0d4c3f83caf362da9
Muskan Verma <100348028+MuskanVerma11@users.noreply.github.com>
2023-01-21 06:03:18 -0800

    Create facePattern.java

---
+++ Patterns/facePattern.java
@@ -0,0 +1,9 @@
+class facePattern{
+	public static void main(String args[]){
+System.out.println(" +\"\"\"\"\"+ ");                                                
+System.out.println("[| o o |]");                                                
+System.out.println(" |  ^  | ");                                                
+System.out.println(" | '-' | ");                                                
+System.out.println(" +-----+ ");
+	}
+}
