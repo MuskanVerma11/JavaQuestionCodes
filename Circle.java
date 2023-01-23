79074b3303c6456807b280a88b3c903d4d6bebef
Muskan Verma <100348028+MuskanVerma11@users.noreply.github.com>
2023-01-23 09:51:52 -0800

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,9 @@
+class Circle{
+	public static void main(String []args){
+		double radius=7.5;
+		double area=Math.PI*radius*radius;
+		double perimeter=2*3.14*radius;
+		System.out.println("Area of circle is " +area);
+		System.out.println("Perimeter of circle is " +perimeter);
+	}
+}
