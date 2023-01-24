da3744b1f2628bbf7a754c000840620e99132634
Muskan Verma <100348028+MuskanVerma11@users.noreply.github.com>
2023-01-24 06:02:30 -0800

    Add files via upload

---
+++ String Operations/stringLength.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class stringLength{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter String : ");
+		String s=sc.nextLine();
+		System.out.println("Length of given string is " +s.length());
+	}
+}
