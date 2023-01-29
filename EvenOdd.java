0ba2e9b739b291fa228b6cd9672c3e93a74d626e
Muskan Verma <100348028+MuskanVerma11@users.noreply.github.com>
2023-01-29 05:43:48 -0800

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class EvenOdd{
+	public static void main(String []args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Number :");
+		int a=sc.nextInt();
+		if(a%2==0)
+		System.out.println("Even Number");
+		else System.out.println("Odd Number");		
+	}
+}
