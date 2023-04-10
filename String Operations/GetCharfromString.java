c504a8323c3b3fc5d225eb2051800c1e1336ffb4
Muskan Verma <100348028+MuskanVerma11@users.noreply.github.com>
2023-04-10 09:57:09 -0700

    Add files via upload

---
+++ String Operations/GetCharfromString.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class GetCharfromString{
+	public static void main(String [] args){
+		String str="Programming";
+		System.out.println("character at index 4 is " +str.charAt(4));
+	}
+}
